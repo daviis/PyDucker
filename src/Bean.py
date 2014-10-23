a4acc4bd245e2225c2394f10fd95cacbe3862356
daviis <isaac@luther.edu>
2014-10-22 19:50:08 -0500

    Made the beans more pythonic. Added LevelBean to deal with scoping

--- src/Bean.py
+++ src/Bean.py
@@ -1,73 +1,87 @@
-class ClassDefBean():
-    
-    def __init__(self, classname):
-        self.classname = classname
-        self.varinfo = {}
-        self.FunDefobject = []
+"""
+All classes contained within are wrappers around instance data dealing with ast traversal
 
-    def classname(self):
-        return self.classname
+@author: Emily 2014/10/20
+revised: Isaac 2014/10/22
+"""
+import sys
 
-    def varinfo(self):
-        return self.varinfo
-
-    def addvarinfo(self, varname, vartype):
-        self.varinfo[varname] = vartype
-    
-    def FunDefobject(self):
-        return self.FunDefobject
-    
-    def addFunDefobject(self, FunDefobject):
-        self.FunDefobject.append(FunDefobject)
-
-class FunDefBean():
-    
-    def __init__(self, returntype, fundefname):
-        self.numparams = 0
-        self.typesparams = []
-        self.returntype = returntype
-        self.fundefname = fundefname
-
-        
-    def numparams(self):
-        return(self.numparams)
-
-    def addnumparams(self):
-        self.numparams = self.numparams + 1 
-
-    def typesparmas(self):
-        return self.typesparmas
-    
-    def addtypesparams(self, typeparam):
-        self.typesparams.append(typeparam)
-
-    def returntype(self):
-        return self.returntype
-    
-    def fundefname(self):
-        return self.fundefname
-
-class VarBean():
-    
-    def __inti__(self, varname, vartype):
-        self.varname = varname
-        self.vartype = vartype
-        
-    def varname(self):
-        return self.varname
-
-    def vartype(self):
-        return self.vartype
-
-    
 class GenericBean():
     
     def __inti__(self, name, initialize):
+        """
+        @name:str
+        @initalize:bool
+        """
         self.name = name
-        self.initialize
+        self.initialize = initialize
 
-    def name(self):
-        return self.name
+class ClassDefBean(GenericBean):
+    
+    def __init__(self, classname, selfVars):
+        """
+        @className:str
+        @selfVars:VarBean*
+        """
+        self.name = classname
+        self.varinfo = LevelBean(selfVars)
+        self.FunDefArr = {} #of key = Fundef.name, FunDefBean for faster look up
 
-    def initialize(self):
-        return self.initialize
+
+class FunDefBean(GenericBean):
+    
+    def __init__(self, paramsTypes, returntype, fundefname):
+        """
+        @paramsTypes:VarBean*
+        @returnType:VarBean
+        @fundefname:str
+        """
+        self.typesparams = LevelBean(paramsTypes)
+        self.returnType = returntype
+        self.name = fundefname
+        self.numparams = len(paramsTypes) # this should be assigned after creation to be length of self.typesparams
+
+
+class VarBean(GenericBean):
+    
+    def __inti__(self, name, aType):
+        """
+        @name:str
+        @aType:str
+        """
+        self.varname = name
+        self.vartype = aType
+
+
+class LevelBean(GenericBean):
+    
+    def __inti__(self, incomingVars = None):
+        """
+        
+        """
+        self.vars = {} #key will be the varName, value will be the VarBeanReference 
+        if incomingVars:
+            for var in incomingVars:
+                self.append(var)
+        
+    def __getitem__(self, item):
+        """
+        @item:VarBean
+        """
+        return self.vars[item.name]
+    
+    def __conains__(self, item):
+        """
+        @item:VarBean
+        """
+        return item.name in self.vars
+        
+    def append(self, item):
+        """
+        @item:VarBean
+        """
+        if(item.name in self.vars):
+            print("reassign of " + item.name, file=sys.stderr)
+        self.vars[item.name] = item
+        
+        
