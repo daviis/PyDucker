d89efd20a8a94291cbbe8d2664d3b61b81989c0a
daviis <isaac@luther.edu>
2014-11-27 09:29:55 -0600

    Fixed the import in NameSpaceBeans (it was doing both from...import and referencing the internal classes with module.). Fixed the return type to just be a string instead of a tuple, in addition updated the signature of FunDefBean in Bean.py. Passing to Emily to explain what the list of tuples is doing.

--- src/NameSpaceBeans.py
+++ src/NameSpaceBeans.py
@@ -1,60 +1,66 @@
-# FunDefBean
-from Bean import NameSpaceBean, ClassDefBean , FunDefBean, VarBean
+"""
+Created Nov-25-2014
+
+@author: Emily
+@editor: Isaac
+
+A module that hand makes a namespace which will be used in early testing of PyDucker walking. It generates ClassDefBeans for ints and strs and some of the
+FunDefBeans that are inside them.
+"""
+from Bean import NameSpaceBean, ClassDefBean , FunDefBean
+
 def handMakeNameSpace():
-    nameSpace = Bean.NameSpaceBean()
-    intClass = Bean.ClassDefBean()
-    intClass.FunDefArr['__add__'] = Bean.FunDefBean([('self', 'int'),('value', 'int')], ('returntype' , 'int'), '__add__')
-    intClass.FunDefArr['__bool__'] = Bean.FunDefBean([('self' , 'int')] ,('returntype' , 'int'), '__bool__')
-    intClass.FunDefArr['__eq__'] = Bean.FunDefBean([('self', 'int'), ('value' , 'int')],('returntype' , 'bool')  , '__eq__')
-    intClass.FunDefArr['__float__'] = Bean.FunDefBean([('self', 'int')], ('returntype' ,'float'), '__float__')
-    intClass.FunDefArr['__floordiv__'] = Bean.FunDefBean([('self', 'int'), ('value' , 'int')], ('returntype' , 'int'), '__floordiv__')
-    intClass.FunDefArr['__ge__'] = Bean.FunDefBean([('self' , 'int'),('value' , 'int' )], ('returntype', 'bool'), '__ge__')
-    intClass.FunDefArr['__gt__'] = Bean.FunDefBean([('self' , 'int'),('value' , 'int')], ('returntype' , 'bool'), '__gt__')
-    intClass.FunDefArr['__invert__'] = Bean.FunDefBean([('self', 'int')], ('returntype' , 'int'), '__invert__')
-    intClass.FunDefArr['__le__'] = Bean.FunDefBean([('self' , 'int') , ('value' , 'int')] , ('returntype' , 'bool') , '__le__')
-    intClass.FunDefArr['__lshift__'] = Bean.FunDefBean([('self' , 'int'), ('value' , 'int')] , ('returntype' , 'int') , '__lshift__')
-    intClass.FunDefArr['__lt__'] = Bean.FunDefBean([('self' , 'int') , ('value' , 'int')] , ('returntype' , 'bool') , '__lt__')
-    intClass.FunDefArr['__mul__'] = Bean.FunDefBean([('self' , 'int') , ('value' , 'int')] , ('returntype' , 'int') , '__mul__')
-    intClass.FunDefArr['__rshift__'] = Bean.FunDefBean([('self' , 'int'), ('value' , 'int')] , ('returntype' , 'int') , '__rshift__')
-    intClass.FunDefArr['__xor__'] = Bean.FunDefBean([('self' , 'int') , ('value' ,'int')] , ('returntype', 'int') , '__xor__')
-    intClass.FunDefArr['__str__'] = Bean.FunDefBean([('self' , 'int')] , ('returntype' , 'str') , '__str__')
-    intClass.FunDefArr['__sub__'] = Bean.FunDefBean([('self', 'int') , ('value' , 'int')] , ('returntype' , 'int') , '__sub__')
+    nameSpace = NameSpaceBean()
+    intClass = ClassDefBean()
+    intClass.FunDefArr['__add__'] = FunDefBean([('self', 'int'),('value', 'int')],  'int', '__add__')
+    intClass.FunDefArr['__bool__'] = FunDefBean([('self' , 'int')] , 'int', '__bool__')
+    intClass.FunDefArr['__eq__'] = FunDefBean([('self', 'int'), ('value' , 'int')], 'bool' , '__eq__')
+    intClass.FunDefArr['__float__'] = FunDefBean([('self', 'int')], 'float', '__float__')
+    intClass.FunDefArr['__floordiv__'] = FunDefBean([('self', 'int'), ('value' , 'int')],  'int', '__floordiv__')
+    intClass.FunDefArr['__ge__'] = FunDefBean([('self' , 'int'),('value' , 'int' )], ('returntype', 'bool'), '__ge__')
+    intClass.FunDefArr['__gt__'] = FunDefBean([('self' , 'int'),('value' , 'int')],  'bool', '__gt__')
+    intClass.FunDefArr['__invert__'] = FunDefBean([('self', 'int')], 'int', '__invert__')
+    intClass.FunDefArr['__le__'] = FunDefBean([('self' , 'int') , ('value' , 'int')], 'bool', '__le__')
+    intClass.FunDefArr['__lshift__'] = FunDefBean([('self' , 'int'), ('value' , 'int')] ,  'int', '__lshift__')
+    intClass.FunDefArr['__lt__'] = FunDefBean([('self' , 'int') , ('value' , 'int')] ,  'bool', '__lt__')
+    intClass.FunDefArr['__mul__'] = FunDefBean([('self' , 'int') , ('value' , 'int')] ,  'int', '__mul__')
+    intClass.FunDefArr['__rshift__'] = FunDefBean([('self' , 'int'), ('value' , 'int')] ,  'int', '__rshift__')
+    intClass.FunDefArr['__xor__'] = FunDefBean([('self' , 'int') , ('value' ,'int')] , 'int', '__xor__')
+    intClass.FunDefArr['__str__'] = FunDefBean([('self' , 'int')] ,  'str', '__str__')
+    intClass.FunDefArr['__sub__'] = FunDefBean([('self', 'int') , ('value' , 'int')] ,  'int', '__sub__')
     nameSpace['int'] = intClass
     
     
     
-    strClass = Bean.ClassDefBean()
-    strClass.FunDefArr['__add__'] = Bean.FunDefBean([('self' , 'str') , ('value' , 'str') ] , ('returntype' , 'str') , '__add__')
-    strClass.FunDefArr['__contains__'] = Bean.FunDefBean([('self' ,'str') , ('key' , 'str')] , ('returntype' , 'bool'), '__contains__')
-    strClass.FunDefArr['__eq__'] = Bean.FunDefBean([('self' , 'str') , ('value' , 'str')] , ('returntype' , 'bool') , '__eq__')
-    strClass.FunDefArr['__ge__'] = Bean.FunDefBean([('self' , 'str') , ('value' , ' str')] , ('returntype' , 'bool') , '__ge__')
-    strClass.FunDefArr['__gt__'] = Bean.FunDefBean([('self' , 'str') , ('value' , 'str')] , ('returntype' , 'bool') , '__gt__')
-    strClass.FunDefArr['__le__'] = Bean.FundDefBean([('self' , 'str'), ('value' , 'str')] , ('returntype' , 'bool'), '__le__')
-    strClass.FunDefArr['__len__'] = Bean.FunDefBean([('self' , 'str')], ('returntype' , 'int') , '__len__')
-    strClass.FunDefArr['__mul__'] = Bean.FunDefBean([('self' , 'str') , ('value' , 'int')] , ('returntype', 'str') , '__mul__')
-    strClass.FunDefArr['__ne__'] = Bean.FunDefBean([('self', 'str') ,('value' ,'str')] , ('returntype' , 'bool') , '__ne__')
-    strClass.FunDefArr['casefold'] = Bean.FunDefBean([('self', 'str')] , ('returntype', 'str'), 'casefold')
-    strClass.FunDefArr['center'] = Bean.FunDefBean([('self' , 'str') , ('width' , 'int')] , ('returntype' , 'str') , 'center')
-    strClass.FunDefArr['encode'] = Bean.FunDefBean([('self' , 'str'), ('encoding' , 'str') , ('errors' , 'str')] , ('returntype' , 'bool') , 'encode')
-    strClass.FunDefArr['find'] = Bean.FunDefBean([('self' , 'str'), ('sub' , 'str'), ('start', 'int'),('end' ,'int')], ('returntype' , 'int'), 'find')
-    strClass.FunDefArr['index'] = Bean.FunDefBean([('self' , 'str') ,('sub' ,'str'),('start' , 'int'), ('end' , 'int')] ,('returntype' , 'int'), 'index')
-    strClass.FunDefArr['isdecimal'] = Bean.FunDefBean([('self' , 'str')] , ('returntype' , 'bool') , 'isdecimal')
-    strClass.FunDefArr['isdigit'] = Bean.FunDefBean([('self' , 'str')] , ('returntype' , 'bool') , 'idigit')
-    strClass.FunDefArr['islower'] = Bean.FunDefBean([('self' , 'str')] , ('returntype' , 'bool') , 'islower')
-
-    strClass.FunDefArr['isnumeric'] = Bean.FunDefBean([('self' , 'str')] , ('returntype' , 'bool') , 'isnumeric')
-    strClass.FunDefArr['isprintable'] = Bean.FunDefBean([('self' , 'st')] , ('returntype' , 'bool') , 'isprintable')
-    strClass.FunDefArr['isspace'] = Bean.FunDefBean([('self' , 'str')] , ('returntype' , 'bool'), 'isspace')
-    strClass.FunDefArr['isupper'] = Bean.FunDefBean([('self' , 'str')] , ('returntype' , 'bool'), 'isupper')
-    strClass.FunDefArr['join'] = Bean.FunDefBean([('self' , 'str')] , ('returntype' , 'str'), 'join')
-    strClass.FunDefArr['lower'] = Bean.FunDefBean([('self' , 'str')] , ('returntype' , 'str'), 'lower')
-    strClass.FunDefArr['replace'] = Bean.FunDefBean([('self' , 'str') , ('old' , 'str'), ('new' , 'str') , ('count' , 'int')] , ('returntype' , 'str') , 'replace')
-    strClass.FunDefArr['split'] = Bean.FunDefBean([('self' , 'str'), ('sep' , 'str') , ('maxsplit' , 'int')] , ('returntype' , 'list') , 'split')
-    strClass.FunDefArr['splitlines'] = Bean.FunDefBean([('self' , 'str') , ('keepends' , 'boolean')] , ('returntype' , 'list') , 'splitlines')
-    strClass.FunDefArr['upper'] = Bean.FunDefBean([('self' , 'str')] , ('returntype' , 'str') , 'upper')
-    
-
+    strClass = ClassDefBean()
+    strClass.FunDefArr['__add__'] = FunDefBean([('self' , 'str') , ('value' , 'str') ] , 'str', '__add__')
+    strClass.FunDefArr['__contains__'] = FunDefBean([('self' ,'str') , ('key' , 'str')] , 'bool', '__contains__')
+    strClass.FunDefArr['__eq__'] = FunDefBean([('self' , 'str') , ('value' , 'str')] , 'bool', '__eq__')
+    strClass.FunDefArr['__ge__'] = FunDefBean([('self' , 'str') , ('value' , ' str')] , 'bool', '__ge__')
+    strClass.FunDefArr['__gt__'] = FunDefBean([('self' , 'str') , ('value' , 'str')] , 'bool', '__gt__')
+    strClass.FunDefArr['__le__'] = FunDefBean([('self' , 'str'), ('value' , 'str')] , 'bool', '__le__')
+    strClass.FunDefArr['__len__'] = FunDefBean([('self' , 'str')], 'int', '__len__')
+    strClass.FunDefArr['__mul__'] = FunDefBean([('self' , 'str') , ('value' , 'int')] , 'str', '__mul__')
+    strClass.FunDefArr['__ne__'] = FunDefBean([('self', 'str') ,('value' ,'str')] , 'bool' , '__ne__')
+    strClass.FunDefArr['casefold'] = FunDefBean([('self', 'str')] , 'str', 'casefold')
+    strClass.FunDefArr['center'] = FunDefBean([('self' , 'str') , ('width' , 'int')] , 'str', 'center')
+    strClass.FunDefArr['encode'] = FunDefBean([('self' , 'str'), ('encoding' , 'str') , ('errors' , 'str')] , 'bool' , 'encode')
+    strClass.FunDefArr['find'] = FunDefBean([('self' , 'str'), ('sub' , 'str'), ('start', 'int'),('end' ,'int')], 'int', 'find')
+    strClass.FunDefArr['index'] = FunDefBean([('self' , 'str') ,('sub' ,'str'),('start' , 'int'), ('end' , 'int')] , 'int', 'index')
+    strClass.FunDefArr['isdecimal'] = FunDefBean([('self' , 'str')] ,  'bool', 'isdecimal')
+    strClass.FunDefArr['isdigit'] = FunDefBean([('self' , 'str')] ,  'bool', 'idigit')
+    strClass.FunDefArr['islower'] = FunDefBean([('self' , 'str')] ,  'bool', 'islower')
+    strClass.FunDefArr['isnumeric'] = FunDefBean([('self' , 'str')] ,  'bool', 'isnumeric')
+    strClass.FunDefArr['isprintable'] = FunDefBean([('self' , 'st')] ,  'bool', 'isprintable')
+    strClass.FunDefArr['isspace'] = FunDefBean([('self' , 'str')] ,  'bool', 'isspace')
+    strClass.FunDefArr['isupper'] = FunDefBean([('self' , 'str')] ,  'bool', 'isupper')
+    strClass.FunDefArr['join'] = FunDefBean([('self' , 'str')] ,  'str', 'join')
+    strClass.FunDefArr['lower'] = FunDefBean([('self' , 'str')] ,  'str', 'lower')
+    strClass.FunDefArr['replace'] = FunDefBean([('self' , 'str') , ('old' , 'str'), ('new' , 'str') , ('count' , 'int')] ,  'str', 'replace')
+    strClass.FunDefArr['split'] = FunDefBean([('self' , 'str'), ('sep' , 'str') , ('maxsplit' , 'int')] ,  'list', 'split')
+    strClass.FunDefArr['splitlines'] = FunDefBean([('self' , 'str') , ('keepends' , 'boolean')] ,  'list', 'splitlines')
+    strClass.FunDefArr['upper'] = FunDefBean([('self' , 'str')] ,  'str', 'upper')
                                                 
                                                     
-    nameSpace['str'] = strclass
+    nameSpace['str'] = strClass
     return nameSpace
