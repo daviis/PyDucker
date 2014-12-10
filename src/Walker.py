52b9a6ce1bc11fdad3997640aee545ff9c59f9de
daviis <isaac@luther.edu>
2014-12-09 20:33:36 -0600

    Implemented the visit_* to get while loops working. This meant visit_while, visit_compare, and a few other. I added a bool class to the NameSpaceBean builder and one fun def.

--- src/Walker.py
+++ src/Walker.py
@@ -47,7 +47,7 @@ class InitialWalker(ast.NodeVisitor):
         """
         if isinstance(node, ast.AST):
             print("Unknown type of ast node. Need to implement visit_" + node.__class__.__name__)
-            
+            super().generic_visit(node)
         #set a break point on this to find where there is a need to list-ify a vist_
         elif isinstance(node, list):
             print('got a list')
@@ -64,19 +64,13 @@ class InitialWalker(ast.NodeVisitor):
         Usually a binop will call this and return a str rep of the magic method __add__
         """
         return "__add__"
+    
+    def visit_And(self, node):
+        """
+        @node:ast.ast
+        """
+        return "__and__"
      
-    def visit_arg(self, node):
-        for _, value in ast.iter_fields(node):
-            self.visit(value)
-            
-    def visit_arguments(self, node):
-        for _, value in ast.iter_fields(node):
-            if isinstance(value, list):
-                for arg in value:
-                    self.visit(arg)
-            elif value:
-                self.visit(value)
-                
     def visit_Assign(self, node):
         """
         @node:ast.ast
@@ -107,30 +101,6 @@ class InitialWalker(ast.NodeVisitor):
         ctx = self.visit(node.ctx)
         return value, node.attr
             
-    def visit_AugAssign(self, node):
-        for _, value in ast.iter_fields(node):
-            self.visit(value)
-        
-    def visit_UnaryOp(self,node):
-        operand = self.visit(node.operand)
-        operandBean = self.nameSpace[operand]
-        op = self.visit(node.op)
-        if operandBean.hasFun(op):
-            return operandBean.funs[op].returnType
-        else:
-            #Need an exception for unary ops
-            #Exceptions.MissingMagicMethodException(operandBean.name, op, node.lineno)
-            print('Error found when trying to '+ op + ' on ' + operand +'.')#,file = sys.stderr)
-    
-    def visit_Invert(self,node):
-        return('__invert__')
-    
-    def visit_UAdd(self,node):
-        return('__pos__')
-    
-    def visit_USub(self,node):
-        return('__neg__')
-           
     def visit_BinOp(self, node):
         """
         This will return the type of the function that is being called. Could throw MissingMethodException if the magic method cant be found.
@@ -156,6 +126,21 @@ class InitialWalker(ast.NodeVisitor):
         else:
             raise Exceptions.MissingMagicMethodException(leftBean, rightBean, op, rOp, node.lineno)
          
+    def visit_BoolOp(self, node):
+        """
+        @node:ast.ast
+        We have the chance to find unreachable code here. It isn't necessary to keep track of op if we dont.
+        """
+        op = self.visit(node.op)
+        valueList = []
+        for val in node.values:
+            valueList.append(self.visit(val))
+            
+        for type in valueList:
+            if not self.nameSpace[type].hasFun('__bool__'): #make sure that object can be evaluated as a boolean
+                raise Exceptions.MissingMagicMethodException(node.lineno, self.nameSpace[type]) #need to make a unop magic method excception
+        return 'bool'
+    
     def visit_Call(self, node):
         """
         When a method is called on a class it generates one of these. This will attempt to return the str rep of the return type of the function.
@@ -182,22 +167,65 @@ class InitialWalker(ast.NodeVisitor):
         else:
             raise Exceptions.MissingMethodException(clsBean, funcName, node.lineno)
  
-    def visit_Module(self, node):
-        for _, value in ast.iter_fields(node):
-            for item in value:
-                self.visit(item)
-             
+    def visit_Compare(self, node):
+        """
+        @node:ast.ast
+        Need to be able to handle 1 < 2 < 3 as well as (1 < 2) < 3
+        """
+        left = self.visit(node.left)
+        
+        opList = []
+        for op in node.ops:
+            opList.append(self.visit(op))
+        
+        compsList = []
+        for comp in node.comparators:
+            compsList.append(self.visit(comp))
+        
+        for idx in range(len(compsList)):
+            leftClass = self.nameSpace[left]
+            right = compsList[idx]
+            op = opList[idx]
+            
+            if leftClass.hasFun(op):
+                if leftClass.funs[op].takes([right]):
+                    left = right
+                else:
+                    raise Exceptions.IncorrectMethodExcepiton(leftClass, op, right, node.lineno)
+            else:
+                raise Exceptions.MissingMethodException(leftClass, op, node.lineno)
+            
+        return 'bool'
+    
+    def visit_Eq(self, node):
+        """
+        @node:ast.ast        
+        """
+        return "__eq__"
+    
     def visit_Expr(self, node):
-        for _, value in ast.iter_fields(node):
-            self.visit(value)
-             
-    def visit_For(self, node):
-        for _, value in ast.iter_fields(node):
-            if isinstance(value, list):
-                for arg in value:
-                    self.visit(arg)
-            elif value:
-                self.visit(value)
+        """
+        @node:ast.ast
+        """
+        return self.visit(node.value)
+ 
+    def visit_Invert(self,node):
+        """
+        @node:ast.ast
+        """
+        return('__invert__')
+    
+    def visit_GtE(self, node):
+        """
+        @node:ast.ast        
+        """
+        return "__ge__"
+    
+    def visit_Lt(self, node):
+        """
+        @node:ast.ast
+        """
+        return "__lt__"
             
     def visit_Load(self, node):
         """
@@ -205,7 +233,11 @@ class InitialWalker(ast.NodeVisitor):
         This is an empty ast node, it just denodes that the name will be loading from namespace
         """
         return False
-
+    
+    def visit_Module(self, node):
+        for bod in node.body:
+            self.visit(bod)
+    
     def visit_Mult(self, node):
         return "__mul__"
     
@@ -223,6 +255,18 @@ class InitialWalker(ast.NodeVisitor):
             return node.id
         else:    
             return self.scope[node.id].type
+        
+    def visit_NameConstant(self, node):
+        """
+        @node:ast.ast
+        """
+        return type(node.value).__name__
+        
+    def visit_NotEq(self, node):
+        """
+        @node:ast.ast
+        """
+        return "__ne__"
              
     def visit_Num(self, node):
         return type(node.n).__name__
@@ -240,6 +284,33 @@ class InitialWalker(ast.NodeVisitor):
             
     def visit_Str(self, node):
         return "str"
+    
+    def visit_UAdd(self,node):
+        return('__pos__')
+    
+    def visit_UnaryOp(self,node):
+        operand = self.visit(node.operand)
+        operandBean = self.nameSpace[operand]
+        op = self.visit(node.op)
+        if operandBean.hasFun(op):
+            return operandBean.funs[op].returnType
+        else:
+            #Need an exception for unary ops
+            #Exceptions.MissingMagicMethodException(operandBean.name, op, node.lineno)
+            print('Error found when trying to '+ op + ' on ' + operand +'.')#,file = sys.stderr)
+
+    def visit_USub(self,node):
+        return('__neg__')
+    
+    def visit_While(self, node):
+        """
+        @node:ast.ast
+        """
+        self.visit(node.test)
+#         for bodyPart in node.body:
+#             self.visit(bodyPart) #i dont think this needs to store what gets returned
+#         for orelse in node.orelse:
+#             self.visit(orelse)
      
     #These are initial walker independent, ie they should be over written in inheriting classes    
     def visit_ClassDef(self, node):
