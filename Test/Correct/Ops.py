ca6f8d38b62115414700ec7584878db7c7f2263f
daviis <isaac@luther.edu>
2015-02-10 16:39:07 -0600

    Add Ops.py from addedTests branch.

---
+++ Test/Correct/Ops.py
@@ -0,0 +1,30 @@
+#BinOp, UnOp, BoolOp as well as operators themselves
+#Covers or, and UAdd, USub, Invert, Not, 
+#Add, Sub, Div, Mod, Pow, LShift, RShift, floordiv
+#Bitwise next BitOr
+import operator
+
+1 and 2
++(1)
+-(1)
+~(1)
+not 1
+1+1
+1-1
+1*1
+1/1
+1%1
+1**1
+1<<1
+1>>1
+1//1
+1|1
+1&1
+1^1
+1>2
+1>=2
+1<2
+1<=2
+1==1
+1!=1
+'hello'[0]
