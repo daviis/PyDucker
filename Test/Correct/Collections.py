5361ae2748df2dc743bfb06ca84716158b9b7b51
daviis <isaac@luther.edu>
2015-02-12 20:30:05 -0600

    Fixed commented out sections

--- Test/Correct/Collections.py
+++ Test/Correct/Collections.py
@@ -1,12 +1,12 @@
-# {1, 2, 3} #a set
-# { x for x in [1]} #setcomp
-#  
-# {1:'a', 2:'b'} #dict
-# { x : x+1 for x in [1]} #dictcomp
-# 
-# [1, 2, 3] #a list
-# [ x for x in [1]]
-# 
-# a, *b = (1, 2, 3)
+{1, 2, 3} #a set
+{ x for x in [1]} #setcomp
+  
+{1:'a', 2:'b'} #dict
+{ x : x+1 for x in [1]} #dictcomp
+ 
+[1, 2, 3] #a list
+[ x for x in [1]]
+ 
+a, *b = (1, 2, 3)
 
 (x for x in [1])  #generatorexp
--- Test/Correct/Compare.py
+++ Test/Correct/Compare.py
@@ -1,6 +1,6 @@
-# 1 is 1
-# 1 is not 1
-# 
-# assert True
+1 is 1
+1 is not 1
+ 
+assert True
 
 1 if 2 else 3
