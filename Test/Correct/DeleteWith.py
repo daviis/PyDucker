ee0ead73e9be8febab25004b80d99ea4eaf38a8d
Jake Albee <JakeAlbee@gmail.com>
2015-02-15 14:04:29 -0600

    Switched up the DeleteWith.py and With.py files to separate Delete, Del and the With testing

--- Test/Correct/DeleteWith.py
+++ Test/Correct/DeleteWith.py
@@ -1,9 +1,3 @@
-class doStuff():
-    def __enter__(self):
-        pass
-    def __exit__(self,type,value,traceback):
-        with doStuff() as thing:
-            x = 1
 y = [1,2,3]
 z = '1'
 del(y[0])
