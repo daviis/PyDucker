73e4f74b3894a8c48707c4036a58c7155bb8f56e
daviis <isaac@luther.edu>
2015-02-17 15:44:38 -0600

    Reorder test for collections.py

--- Test/Correct/Collections.py
+++ Test/Correct/Collections.py
@@ -1,6 +1,9 @@
+(1, 2, 3) #tuple
+(x for x in [1,2])  #generatorexp
+
 # {1, 2, 3} #a set
 { x for x in [1]} #setcomp
-# { x for x in [1] if x < 0} #setcomp with an if to test visit_comprehension
+{ x for x in [1] if x < 0} #setcomp with an if to test visit_comprehension
   
 {1:'a', 2:'b'} #dict
 { x : x+1 for x in [1]} #dictcomp
@@ -8,6 +11,4 @@
 [1, 2, 3] #a list
 [ x for x in [1]]
  
-a, *b = (1, 2, 3)
-
-(x for x in [1])  #generatorexp
+a, *b = (1, 2, 3) #starred and tuple unpacking
