12bd41698ac128f72d20574cba69ef42db893398
daviis <isaac@luther.edu>
2015-02-18 18:05:57 -0600

    Uncomment all of the tests, add assignments to every line, add pass to eof so for testing add breakpoint in visit_pass and look into self.scope

--- Test/Correct/Collections.py
+++ Test/Correct/Collections.py
@@ -1,15 +1,15 @@
-# (1, 2, 3) #tuple
-# (x for x in [1,2])  #generatorexp
-# 
-# # {1, 2, 3} #a set
-# { x for x in [1]} #setcomp
-# { x for x in [1] if x < 0} #setcomp with an if to test visit_comprehension
-#   
-# {1:'a', 2:'b'} #dict
-# { x : x+1 for x in [1]} #dictcomp
-#  
-# [1, 2, 3] #a list
-# [ x for x in [1]]
+aTuple = (1, 2, 3) #tuple
+aTupleComp = (x for x in [1,2])  #generatorexp
  
-a, *b = (1, 2, 3) #starred and tuple unpacking
-c =1
+aSet = {1, 2, 3} #a set
+aSetComp = { x for x in [1]} #setcomp
+aSetCompIf = { x for x in [1] if x < 0} #setcomp with an if to test visit_comprehension
+   
+aDict = {1:'a', 2:'b'} #dict
+aDictComp = { x : x+1 for x in [1]} #dictcomp
+  
+aList = [1, 2, 3] #a list
+aListComp = [ x for x in [1]]
+ 
+nonStarred, *aStarred = (1, 2, 3) #starred and tuple unpacking
+pass
