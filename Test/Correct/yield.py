f23aeee90d5237d279cf655da8c4f6c67234b244
Emily <eplatts14@gmail.com>
2015-03-06 07:59:10 -0600

    changed range functions to a list

--- Test/Correct/yield.py
+++ Test/Correct/yield.py
@@ -1,8 +1,9 @@
-values = [(yield from (i, i + 1, i)) for i in range(5)]
+
+values = [(yield from (i, i + 1, i)) for i in [0,1,2,3,4]]
 
 def _tmpfunc():
-    for x in range(5): 
+    for x in [0,1,2,3,4]: 
         yield (yield from (i, i + 1, i))
 
-values = list(_tmpfunc())
+#values = list(_tmpfunc())
 
