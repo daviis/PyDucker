f7c722d266c830485ead978104796517c24defd8
Emily <eplatts14@gmail.com>
2015-03-10 14:38:52 -0500

    add way for comand line parser to send files to TestASt

--- src/pyducker.py
+++ src/pyducker.py
@@ -5,16 +5,23 @@ Created on Sep 18, 2014
 '''
 import sys
 import argparse
+from TestAST import testFiles
 
 def main():
     parser = argparse.ArgumentParser(description="A static analysis tool for Python 3.x")
-    parser.add_argument('files', metavar='F', type=str, nargs='+',
+    parser.add_argument('files', metavar='F', type=open, nargs='+',
                    help='an file to statically check')
     parser.add_argument('-n', dest='accumulate', action='store_const',
                    const=sum, default=max,
                    help='check the variable names in the file only')
+    parser.add_argument('-w', help = 'turns of PyDucker warnings')
     args = parser.parse_args()
     
+    if args.files:
+        testFiles(args.files)
+        
+
+
 
 if __name__ == '__main__':
     main()
