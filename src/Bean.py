a7be1b7f153bf2c66b42cd2ebb71a05b2baca0a0
Emily <eplatts14@gmail.com>
2015-03-24 10:39:35 -0500

    seeing how to fix f RefBeforAssignException error on line 3

--- src/Bean.py
+++ src/Bean.py
@@ -41,8 +41,6 @@ class ClassDefBean(GenericBean):
         @fun:FunDefBean
         """
         try:
-            print(fun.name)
-            print(self.funs[fun.name])
             if self.funs[fun.name] == fun:
                 return self.funs[fun.name].returnType 
         except KeyError:
