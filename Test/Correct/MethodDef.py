a4d019e235b6f38af8f62c831d53fe4b4e1e70aa
daviis <isaac@luther.edu>
2015-03-25 21:09:49 -0500

    Set up testing files.

--- Test/Correct/MethodDef.py
+++ Test/Correct/MethodDef.py
@@ -1,16 +1,20 @@
-def add(a, b):
+# def add(a, b):
+#     '''
+#     @a:float
+#     @b:float
+#     '''
+#     return a + b 
+# 
+def sub(a, b=666, c="w", *d, **e):
     '''
     @a:float
-    @b:float
+    @b:int
+    @d:str*
+    @e:int**
     '''
-    return a + b 
+    return a - b 
 
-#def sub(a, b):
-  #  '''
- #   @a:float
-  #  @b:float
-   # '''
-    #return a - b 
+pass
 
-def noPrams():
-    return 5
+# def noPrams():
+#     return 5
